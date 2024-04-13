460f0712fafc465d08d525fe27fee54852867a38
pcasaretto <pcasaretto@gmail.com>
2024-04-13 19:21:34 -0300

    implement BIT family

--- src/instructions/bit.rs
+++ src/instructions/bit.rs
@@ -26,6 +26,35 @@ pub fn set_r(target: RegisterTarget, bit_position: u8) -> impl Fn(&mut Gameboy) 
     }
 }
 
+pub(crate) fn bit_r(target: RegisterTarget, bit_position: u8) -> impl Fn(&mut Gameboy) -> u8 {
+    move |gameboy: &mut Gameboy| {
+        let value = gameboy.cpu.registers.get_u8(target);
+        let result = value & (1 << bit_position);
+
+        gameboy.cpu.registers.f.zero = result == 0;
+        gameboy.cpu.registers.f.subtract = false;
+        gameboy.cpu.registers.f.half_carry = true;
+
+        const CYCLES: u8 = 8;
+        CYCLES
+    }
+}
+
+pub(crate) fn bit_mem_at_hl(bit_position: u8) -> impl Fn(&mut Gameboy) -> u8 {
+    move |gameboy: &mut Gameboy| {
+        let addr = gameboy.cpu.registers.get_u16(Register16bTarget::HL);
+        let value = gameboy.bus.read_byte(addr);
+        let result = value & (1 << bit_position);
+
+        gameboy.cpu.registers.f.zero = result == 0;
+        gameboy.cpu.registers.f.subtract = false;
+        gameboy.cpu.registers.f.half_carry = true;
+
+        const CYCLES: u8 = 16;
+        CYCLES
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,5 +76,47 @@ mod tests {
         gameboy.cpu.registers.set_u8(RegisterTarget::A, 0b00000000);
         set_r(RegisterTarget::A, 3)(&mut gameboy);
         assert_eq!(gameboy.cpu.registers.get_u8(RegisterTarget::A), 0b00001000);
+    }
+
+    #[test]
+    fn test_bit_r() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.set_u8(RegisterTarget::A, 0b00001000);
+        bit_r(RegisterTarget::A, 3)(&mut gameboy);
+        assert_eq!(gameboy.cpu.registers.f.zero, false);
+        assert_eq!(gameboy.cpu.registers.f.subtract, false);
+        assert_eq!(gameboy.cpu.registers.f.half_carry, true);
+    }
+
+    #[test]
+    fn test_bit_r_true() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.set_u8(RegisterTarget::A, 0b00000000);
+        bit_r(RegisterTarget::A, 3)(&mut gameboy);
+        assert_eq!(gameboy.cpu.registers.f.zero, true);
+        assert_eq!(gameboy.cpu.registers.f.subtract, false);
+        assert_eq!(gameboy.cpu.registers.f.half_carry, true);
+    }
+
+    #[test]
+    fn test_bit_mem_at_hl() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.set_u16(Register16bTarget::HL, 0xC050);
+        gameboy.bus.write_byte(0xC050, 0b00001000);
+        bit_mem_at_hl(3)(&mut gameboy);
+        assert_eq!(gameboy.cpu.registers.f.zero, false);
+        assert_eq!(gameboy.cpu.registers.f.subtract, false);
+        assert_eq!(gameboy.cpu.registers.f.half_carry, true);
+    }
+
+    #[test]
+    fn test_bit_mem_at_hl_true() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.set_u16(Register16bTarget::HL, 0xC050);
+        gameboy.bus.write_byte(0xC050, 0b00000000);
+        bit_mem_at_hl(3)(&mut gameboy);
+        assert_eq!(gameboy.cpu.registers.f.zero, true);
+        assert_eq!(gameboy.cpu.registers.f.subtract, false);
+        assert_eq!(gameboy.cpu.registers.f.half_carry, true);
     }
 }
