68a0fdb6e5a6523cd697aef2d9c657243582e9e6
pcasaretto <pcasaretto@gmail.com>
2024-04-20 21:08:20 -0300

    fix CP flags

--- src/instructions/cp.rs
+++ src/instructions/cp.rs
@@ -5,6 +5,9 @@ pub fn cp_d8(gameboy: &mut Gameboy) -> u8 {
     let value = gameboy.read_next_byte();
     let a = gameboy.cpu.registers.get_u8(RegisterTarget::A);
     gameboy.cpu.registers.f.zero = a == value;
+    gameboy.cpu.registers.f.subtract = true;
+    gameboy.cpu.registers.f.half_carry = (a & 0x0F) < (value & 0x0F);
+    gameboy.cpu.registers.f.carry = a < value;
     const TICKS: u8 = 8;
     TICKS
 }
@@ -14,6 +17,9 @@ pub fn cp(target: RegisterTarget) -> impl Fn(&mut Gameboy) -> u8 {
         let a = gameboy.cpu.registers.get_u8(RegisterTarget::A);
         let value = gameboy.cpu.registers.get_u8(target);
         gameboy.cpu.registers.f.zero = a == value;
+        gameboy.cpu.registers.f.subtract = true;
+        gameboy.cpu.registers.f.half_carry = (a & 0x0F) < (value & 0x0F);
+        gameboy.cpu.registers.f.carry = a < value;
         const TICKS: u8 = 4;
         TICKS
     }
@@ -25,6 +31,9 @@ pub(crate) fn cp_mem_at_r16(hl: super::Register16bTarget) -> impl Fn(&mut Gamebo
         let addr = gameboy.cpu.registers.get_u16(hl);
         let value = gameboy.bus.read_byte(addr);
         gameboy.cpu.registers.f.zero = a == value;
+        gameboy.cpu.registers.f.subtract = true;
+        gameboy.cpu.registers.f.half_carry = (a & 0x0F) < (value & 0x0F);
+        gameboy.cpu.registers.f.carry = a < value;
         const TICKS: u8 = 8;
         TICKS
     }
@@ -51,6 +60,36 @@ mod tests {
         gameboy.bus.write_byte(gameboy.cpu.registers.pc + 1, 13);
         cp_d8(&mut gameboy);
         assert!(gameboy.cpu.registers.f.zero);
+    }
+
+    #[test]
+    fn test_cp_d8_subtract_flag() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.a = 0x01;
+        gameboy.cpu.registers.pc = 0xC050;
+        gameboy.bus.write_byte(0xC051, 0x01);
+        cp_d8(&mut gameboy);
+        assert!(gameboy.cpu.registers.f.subtract);
+    }
+
+    #[test]
+    fn test_cp_d8_half_carry_flag() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.a = 0x10;
+        gameboy.cpu.registers.pc = 0xC050;
+        gameboy.bus.write_byte(0xC051, 0x01);
+        cp_d8(&mut gameboy);
+        assert!(gameboy.cpu.registers.f.half_carry);
+    }
+
+    #[test]
+    fn test_cp_d8_carry_flag() {
+        let mut gameboy = Gameboy::default();
+        gameboy.cpu.registers.a = 0x00;
+        gameboy.cpu.registers.pc = 0xC050;
+        gameboy.bus.write_byte(0xC051, 0x01);
+        cp_d8(&mut gameboy);
+        assert!(gameboy.cpu.registers.f.carry);
     }
 
     #[test]
